e5ac9df523b616e5dedf36f8b0734799224af9c6
HmonWutt <wtthumon@gmail.com>
2026-02-20 22:20:17 +0100

    added test for text_to_textnodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,13 +1,14 @@
 import unittest
 from htmlnode import HTMLNode
 from textnode import TextNode, TextType
-from split_delimiter import split_nodes_delimiter,extract_markdown_images, extract_markdown_links,split_nodes_image, split_nodes_link
+from split_delimiter import split_nodes_delimiter,extract_markdown_images, extract_markdown_links,split_nodes_image, split_nodes_link,text_to_textnodes
 
 class TestHtmlNode(unittest.TestCase):
     def setUp(self):
         self.htmlnode1 = HTMLNode("a","this is a link to google.com", None, {"href":"https://google.com","target": "_blank"})
         self.htmlnode2 = HTMLNode("div", None, [self.htmlnode1], {})
         self.node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        self.maxDiff = None
     def test_props_to_html(self):
         self.assertEqual(f' href="https://google.com" target="_blank"', self.htmlnode1.props_to_html(), '')
 
@@ -186,5 +187,22 @@ class TestHtmlNode(unittest.TestCase):
             new_nodes,
         )
 
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        new_nodes = text_to_textnodes(text)
+        self.assertListEqual(
+        [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ] , new_nodes,
+        )
 if __name__ == "__main__":
     unittest.main()
