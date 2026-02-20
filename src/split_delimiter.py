4f5a896c869551a9cdc5d006388c4c2599cba41f
HmonWutt <wtthumon@gmail.com>
2026-02-20 12:54:39 +0100

    split_delimiter works with multiple character delimiter

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -4,33 +4,23 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for each in old_nodes:
         if each.text_type == TextType.TEXT:
             new_nodes.extend(helper(each,delimiter,text_type))
+        else:
+            new_nodes.append(each)
     return new_nodes
 
 
 def helper(node,delimiter,type):
+    if not node.text:
+        return []
     new_nodes = []
-    delimiter_count = 0
-    temp = ""
-    for char in node.text:
-        if char == delimiter:
-            if delimiter_count==1 and temp:
-                node = TextNode(temp,type)
-                temp = ""
-                delimiter_count=0
-                new_nodes.append(node)
-            elif delimiter_count == 0 and temp:
-                node = TextNode(temp,TextType.TEXT)
-                temp = ""
-                delimiter_count+=1
-                new_nodes.append(node)
+    split_texts = node.text.split(delimiter)
+    for ind, each in enumerate(split_texts):
+        if each:
+            if ind%2 ==0:
+                new_nodes.append(TextNode(each,TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(each,type))
 
-        else:
-            temp+=char
-    if delimiter_count == 1:
-        raise Exception("delimiter unmatched")
-    if temp:
-        node = TextNode(temp, TextType.TEXT)
-        new_nodes.append(node)
     return new_nodes
 
     
