07ce89c52a743dfaa9fbe2f209533937653eaf00
HmonWutt <wtthumon@gmail.com>
2026-02-24 12:26:18 +0100

    added functions to copy files over from source dir to destination dir

--- src/main.py
+++ src/main.py
@@ -1,10 +1,38 @@
 from textnode import TextNode, TextType
-
-
+import os
+import shutil
 def main():
     text_node = TextNode("hello world", TextType.BOLD, "www.dummy.com")
-    print(text_node)
+    os.chdir('/home/hmon/bootdev/github.com/HmonWutt/static_site_generator')
+    filepaths = copy_files('static')
+    paste_files(filepaths,'public')
+    #print(text_node)
 
+def copy_files(src_dir):
+    if not os.path.exists(src_dir):
+        raise Exception("source path not valid")
+    if os.path.isfile(src_dir):
+        return src_dir
+    return helper_copy_files(src_dir,[])
 
+def helper_copy_files(path,file_paths):
+    if os.path.isfile(path):
+        file_paths.append(path)
+        print("src: ",path)
+        return file_paths
+    sub_dirs = os.listdir(path) 
+    for sub_dir in sub_dirs:
+        joined_path = os.path.join(path,sub_dir)
+        helper_copy_files(joined_path, file_paths)
+    return file_paths
+
+def paste_files(filepaths, destination_dir):
+    for filepath in filepaths:
+        src_path = os.path.join(*filepath.split(os.sep)[1:])
+        dest_path = os.path.join(destination_dir,src_path)
+        dir = os.path.dirname(dest_path)
+        os.makedirs(dir,exist_ok=True)
+        shutil.copy(filepath,dest_path)
+        print("dest: ",dest_path)
 main()
 print("see you later")
