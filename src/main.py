bc292b2c14520b1ff7d543bc2dade11a3345ac9c
HmonWutt <wtthumon@gmail.com>
2026-03-03 17:41:53 +0100

    fixed file paths

--- src/main.py
+++ src/main.py
@@ -5,22 +5,17 @@ import sys
 from text_to_blocks import extract_title, copy_file_paths, paste_files, empty_dir, generate_pages_recursively
 def main():
     text_node = TextNode("hello world", TextType.BOLD, "www.dummy.com")
-
-    #if len(sys.argv) != 4:
-    #    print("Usage: python script.py <url> <source_dir> <destination_dir>")
-    #    sys.exit(1)
-    #
-    #url = sys.argv[1]
-    #source_dir = sys.argv[2]
-    #destination_dir = sys.argv[3]
-    #
-    #print("URL:", url)
-    #print("Source:", source_dir)
-    #print("Destination:", destination_dir)
-    os.chdir('/home/hmon/bootdev/github.com/HmonWutt/static_site_generator/')
+    
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <your github repo url>")
+        sys.exit(1)
+    
+    basepath = sys.argv[1]
+    print("Base path:", basepath)
+    #os.chdir('/home/hmon/bootdev/github.com/HmonWutt/static_site_generator/')
     static_filepaths = copy_file_paths('static')
-    empty_dir('public')
-    paste_files(static_filepaths,'public')
-    generate_pages_recursively('content', 'template.html', 'public')
+    empty_dir('docs')
+    paste_files(static_filepaths,'docs')
+    generate_pages_recursively('content', 'template.html', 'docs',basepath)
 main()
-print("see you later")
+print("Done!🎉")
