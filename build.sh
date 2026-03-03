b17cf800ff88d471687fbe89b30b1e0258b0f085
HmonWutt <wtthumon@gmail.com>
2026-03-04 00:48:18 +0100

    main and build scripts

---
+++ build.sh
@@ -0,0 +1,5 @@
+echo "Github pages url: $1"
+echo "Static folder: $2"
+echo "Markdown folder: $3"
+echo "Destination folder: $4"
+python3 src/main.py $1 $2 $3 $4
---
+++ main.sh
@@ -0,0 +1,2 @@
+python3 src/main.py "/" "static" "content" "docs"
+cd docs && python3 -m http.server 8888
